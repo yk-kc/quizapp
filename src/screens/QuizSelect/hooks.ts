df8b54fb8a292aa596fb694b536f874ba63d8091
Kaito Kusaba <40602595+kaito-kusaba@users.noreply.github.com>
2023-06-15 19:37:37 +0900

    refactor: quiz selector component

--- src/screens/QuizSelect/hooks.ts
+++ src/screens/QuizSelect/hooks.ts
@@ -1,6 +1,22 @@
 import type { HomeStackParamList } from '@quizapp/types/routes'
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { useNavigation } from '@react-navigation/native'
+import { ImageSourcePropType } from 'react-native'
+import { Colors } from '@quizapp/constants'
+import jsIcon from '@quizapp/assets/icons/js.png'
+import rbIcon from '@quizapp/assets/icons/rb.png'
+import pyIcon from '@quizapp/assets/icons/py.png'
+import htmlIcon from '@quizapp/assets/icons/html.png'
+import cssIcon from '@quizapp/assets/icons/css.png'
+import sqlIcon from '@quizapp/assets/icons/sql.png'
+
+export type Lang = {
+  title: string
+  logo: ImageSourcePropType
+  onPress: () => void
+  bgcolor: string
+  color?: string
+}
 
 export function useHooks() {
   const navigation = useNavigation<NativeStackNavigationProp<HomeStackParamList, 'home'>>()
@@ -29,7 +45,34 @@ export function useHooks() {
     navigation.navigate('quiz/sql')
   }
 
+  const LANGS: Lang[] = [
+    {
+      title: 'JavaScript',
+      onPress: onPressJS,
+      bgcolor: Colors.JAVASCRIPT,
+      logo: jsIcon,
+    },
+    {
+      title: 'Ruby',
+      onPress: onPressRB,
+      bgcolor: Colors.RUBY,
+      color: Colors.WHITE,
+      logo: rbIcon,
+    },
+    { title: 'Python', onPress: onPressPY, bgcolor: Colors.PYTHON, logo: pyIcon },
+    { title: 'HTML', onPress: onPressHTML, bgcolor: Colors.HTML, logo: htmlIcon },
+    { title: 'CSS', onPress: onPressCSS, bgcolor: Colors.CSS, logo: cssIcon },
+    {
+      title: 'SQL',
+      onPress: onPressSQL,
+      bgcolor: Colors.SQL,
+      color: Colors.WHITE,
+      logo: sqlIcon,
+    },
+  ]
+
   return {
+    LANGS,
     onPressJS,
     onPressRB,
     onPressPY,
--- src/screens/QuizSelect/index.tsx
+++ src/screens/QuizSelect/index.tsx
@@ -1,55 +1,13 @@
 import React from 'react'
-import { Text, View, FlatList, ImageSourcePropType } from 'react-native'
+import { Text, View, FlatList } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useStyles } from './style'
 import SelectLangButton from '@quizapp/components/SelectLangButton'
 import { useHooks } from './hooks'
-import { Colors } from '@quizapp/constants'
-
-import jsIcon from '@quizapp/assets/icons/js.png'
-import rbIcon from '@quizapp/assets/icons/rb.png'
-import pyIcon from '@quizapp/assets/icons/py.png'
-import htmlIcon from '@quizapp/assets/icons/html.png'
-import cssIcon from '@quizapp/assets/icons/css.png'
-import sqlIcon from '@quizapp/assets/icons/sql.png'
-
-export type Lang = {
-  title: string
-  logo: ImageSourcePropType
-  onPress: () => void
-  bgcolor: string
-  color?: string
-}
 
 export default React.memo(function QuizSelectScreen() {
   const style = useStyles()
   const hooks = useHooks()
-
-  const LANGS: Lang[] = [
-    {
-      title: 'JavaScript',
-      onPress: hooks.onPressJS,
-      bgcolor: Colors.JAVASCRIPT,
-      logo: jsIcon,
-    },
-    {
-      title: 'Ruby',
-      onPress: hooks.onPressRB,
-      bgcolor: Colors.RUBY,
-      color: Colors.WHITE,
-      logo: rbIcon,
-    },
-    { title: 'Python', onPress: hooks.onPressPY, bgcolor: Colors.PYTHON, logo: pyIcon },
-    { title: 'HTML', onPress: hooks.onPressHTML, bgcolor: Colors.HTML, logo: htmlIcon },
-    { title: 'CSS', onPress: hooks.onPressCSS, bgcolor: Colors.CSS, logo: cssIcon },
-    {
-      title: 'SQL',
-      onPress: hooks.onPressSQL,
-      bgcolor: Colors.SQL,
-      color: Colors.WHITE,
-      logo: sqlIcon,
-    },
-  ]
 
   return (
     <SafeAreaView style={style.container}>
@@ -58,7 +16,7 @@ export default React.memo(function QuizSelectScreen() {
         <FlatList
           numColumns={2}
           keyExtractor={(item, _) => _.toString()}
-          data={LANGS}
+          data={hooks.LANGS}
           renderItem={({ item }) => (
             <SelectLangButton
               title={item.title}
